65ad7d8235eec33be410e7b874eae054dee8e9b7
Farzaneh Sefidabi <fsefidabi@gmail.com>
2021-02-13 20:20:59 +0330

    corrected asyncTimedCargo

--- examples/rabbitmq-insert.js
+++ examples/rabbitmq-insert.js
@@ -1,7 +1,7 @@
 const startPublisher = require('../lib/rabbitmq/publisher')
 const startConsumer = require('../lib/rabbitmq/consumer')
-const writeIntoDatabase = require('../lib/clickhouse/insert-into-table')
+const insertIntoTable = require('../lib/clickhouse/insert-into-table')
 const randomData = require('../lib/clickhouse/random-data-generator')
 
-startConsumer(writeIntoDatabase)
+startConsumer(insertIntoTable)
 startPublisher(randomData)
